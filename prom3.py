b89b05a47dc595d884b0b4951a529489ee9e97e8
Gabriel <gabivelacard11@gmail.com>
2026-07-18 01:06:14 -0600

    calcular el promedio de tres numeros

---
+++ prom3.py
@@ -0,0 +1,5 @@
+num1 = float(input("Por faovr,escriba el primer numero: "))
+num2 = float(input("Por faovr,escriba el segundo numero: "))
+num3 = float(input("Por faovr,escriba el tercer numero: "))
+prom = (num1 + num2 + num3) / 3
+print("El promedio de los anteriores números es: "+str(prom))
