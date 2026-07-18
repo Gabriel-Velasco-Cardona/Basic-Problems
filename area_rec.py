a2eb6d9c5bd12b7354ff0bcda35c67c9721ccf2a
Gabriel <gabivelacard11@gmail.com>
2026-07-18 00:43:41 -0600

    Area rectangulo

---
+++ area_rec.py
@@ -0,0 +1,4 @@
+base = float(input("Ingrese la base del rectangulo: "))
+altura = float(input("Ingrese la altura del rectangulo: "))
+area = base*altura
+print("El area del rectangulo es de: " + str(area))
